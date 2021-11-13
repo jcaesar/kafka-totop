614abcb7faef5f87544d2f4445e754b1c016a541
Julius Michaelis <michaelis@jp.fujitsu.com>
2021-11-13 15:57:20 +0900

    Unspaghetti a bit

--- src/tui.rs
+++ src/tui.rs
@@ -1,30 +1,28 @@
 use anyhow::{Context, Result};
 use chrono::{DateTime, Datelike, Local, Timelike};
 use itertools::Itertools;
-use std::collections::{BTreeSet, HashSet};
-use std::iter::once;
-use std::sync::Mutex;
+use number_prefix::NumberPrefix;
+use rand::Rng;
 use std::{
-    collections::{hash_map::Entry, BTreeMap, HashMap},
+    collections::{hash_map::Entry, BTreeMap, BTreeSet, HashMap, HashSet},
     io,
-    sync::{mpsc, Arc},
+    iter::once,
+    sync::{
+        mpsc::{self, Receiver},
+        Arc, Mutex,
+    },
     thread,
     time::{Duration, Instant},
 };
-use termion::raw::IntoRawMode;
-use termion::{event::Key, input::TermRead, screen::AlternateScreen};
-use ::tui::{backend::TermionBackend, style::Color};
+use termion::{event::Key, input::TermRead, raw::IntoRawMode, screen::AlternateScreen};
 use ::tui::{
-    style::Style,
-    widgets::{Axis, Dataset},
-};
-use ::tui::{symbols, Terminal};
-use ::tui::{
+    backend::TermionBackend,
+    style::{Color, Style},
+    symbols,
     text::Span,
-    widgets::{Chart, GraphType},
+    widgets::{Axis, Chart, Dataset, GraphType},
+    Terminal,
 };
-use number_prefix::NumberPrefix;
-use rand::Rng;
 
 use crate::*;
 
@@ -34,47 +32,51 @@ struct State {
     query_interval: Duration,
 }
 
-pub(crate) fn run(opts: &Opts) -> Result<()> {
-    let state = Arc::new(State {
-        query_interval: opts.interval,
-        ..State::default()
-    });
-    let (offtx, offrx) = mpsc::sync_channel(1_000_000);
-    let (usertx, userrx) = mpsc::sync_channel(1000);
-    thread::spawn({
-        let state = state.clone();
-        let client = client(opts)?;
-        || query_offsets(state, offtx, client)
-    });
-    thread::spawn({
-        let state = state.clone();
-        let client = client(opts)?;
-        || query_bad(state, client)
-    });
-    let stdout = std::io::stdout().into_raw_mode()?;
-    let stdout = AlternateScreen::from(stdout);
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    thread::spawn(|| input(usertx));
-    let mut data: HashMap<String, HashMap<i32, BTreeMap<Instant, i64>>> = HashMap::new();
-    let mut maxy = 1.0f64;
-    loop {
+struct Scrapes {
+    data: HashMap<String, HashMap<i32, BTreeMap<Instant, i64>>>,
+    offrx: Receiver<Message>,
+}
+
+impl Scrapes {
+    fn scrape(opts: &Opts) -> Result<Self, anyhow::Error> {
+        let state = Arc::new(State {
+            query_interval: opts.interval,
+            ..State::default()
+        });
+        let (offtx, offrx) = mpsc::sync_channel(1_000_000);
+        thread::spawn({
+            let state = state.clone();
+            let client = client(opts)?;
+            || query_offsets(state, offtx, client)
+        });
+        thread::spawn({
+            let state = state.clone();
+            let client = client(opts)?;
+            || query_bad(state, client)
+        });
+        Ok(Self {
+            data: HashMap::new(),
+            offrx,
+        })
+    }
+    fn ingest(&mut self) -> Result<()> {
         loop {
-            match offrx.try_recv() {
+            match self.offrx.try_recv() {
                 Ok(Message::PartitionOffsets {
                     topic,
                     partition,
                     offset,
                     now,
                 }) => {
-                    data.entry(topic)
+                    self.data
+                        .entry(topic)
                         .or_insert_with(HashMap::new)
                         .entry(partition)
                         .or_insert_with(BTreeMap::new)
                         .insert(now, offset);
                 }
                 Ok(msg) => anyhow::bail!("TODO: handle {:?}", msg),
-                Err(mpsc::TryRecvError::Empty) => break,
+                Err(mpsc::TryRecvError::Empty) => return Ok(()),
                 Err(mpsc::TryRecvError::Disconnected) => {
                     // TODO: poll thread exit for an error for a second or so
                     anyhow::bail!("Metadata gatherer bailed");
@@ -82,67 +84,95 @@ pub(crate) fn run(opts: &Opts) -> Result<()> {
             }
         }
         // TODO: Discard old values
+    }
+    fn rates(&self, interval: Duration, buckets: u32) -> (f64, Vec<(&str, Vec<(f64, f64)>, i64)>) {
+        let now = Instant::now();
+        let bucket_size = (interval / buckets).as_secs_f64();
+        let draw_start = now - interval;
+        let mut maxv = 1.0f64;
+        let data = self
+            .data
+            .iter()
+            .map(|(topic, padata)| {
+                let mut buckets = (1 - (buckets as i32)..1)
+                    .map(|idx| (bucket_size * idx as f64, 0f64))
+                    .collect::<Vec<_>>();
+                let mut total = 0;
+                for (_, polls) in padata.iter() {
+                    for ((ai, ao), (bi, bo)) in polls.iter().tuple_windows() {
+                        let diff = bo - ao;
+                        total += diff;
+                        let aedge = ai.checked_duration_since(draw_start);
+                        let bedge = bi.checked_duration_since(draw_start);
+                        let dur = bi.checked_duration_since(*ai);
+                        if let (Some(aedge), Some(bedge), Some(dur)) = (aedge, bedge, dur) {
+                            let aidx = (aedge.as_secs_f64() / bucket_size) as usize;
+                            let bidx = (bedge.as_secs_f64() / bucket_size) as usize;
+                            if aidx == bidx {
+                                if let Some((_, v)) = buckets.get_mut(bidx) {
+                                    *v += diff as f64 / bucket_size;
+                                    maxv = maxv.max(*v);
+                                }
+                            } else {
+                                let rate = diff as f64 / dur.as_secs_f64();
+                                if let Some((_, v)) = buckets.get_mut(aidx) {
+                                    *v += rate
+                                        * ((aidx + 1) as f64 - aedge.as_secs_f64() / bucket_size);
+                                    maxv = maxv.max(*v);
+                                }
+                                if aidx + 1 <= bidx - 1 {
+                                    for (_, v) in buckets[aidx + 1..=bidx - 1].iter_mut() {
+                                        *v += rate;
+                                        maxv = maxv.max(*v);
+                                    }
+                                }
+                                if let Some((_, v)) = buckets.get_mut(bidx) {
+                                    *v += rate * (bedge.as_secs_f64() / bucket_size - bidx as f64);
+                                    maxv = maxv.max(*v);
+                                }
+                            }
+                        }
+                    }
+                }
+                (topic.as_str(), buckets, total)
+            })
+            .filter(|(_, _, total)| *total != 0)
+            .collect::<Vec<_>>();
+        (maxv, data)
+    }
+}
+
+pub(crate) fn run(opts: &Opts) -> Result<()> {
+    let mut scraper = Scrapes::scrape(opts)?;
+    let stdout = std::io::stdout().into_raw_mode()?;
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    let (usertx, userrx) = mpsc::sync_channel(1000);
+    thread::spawn(|| input(usertx));
+    let mut maxy = 1.0f64;
+    loop {
+        scraper.ingest()?;
         terminal.draw(|f| {
             let interval = Duration::from_secs(900); // TODO: CLI option
             let height = f.size().height - 2;
             let width = f.size().width - 7;
             let buckets = width as u32 * 2;
-            let bucket_size = (interval / buckets).as_secs_f64();
-            let now = Instant::now();
             let now_date = Local::now();
-            let draw_start = now - interval;
-            let mut maxv = 1.0f64;
-            let data = data
-                .iter()
-                .map(|(topic, padata)| {
-                    let mut buckets = (1 - (buckets as i32)..1)
-                        .map(|idx| (bucket_size * idx as f64, 0f64))
-                        .collect::<Vec<_>>();
-                    let mut total = 0;
-                    for (_, polls) in padata.iter() {
-                        for ((ai, ao), (bi, bo)) in polls.iter().tuple_windows() {
-                            let diff = bo - ao;
-                            total += diff;
-                            let aedge = ai.checked_duration_since(draw_start);
-                            let bedge = bi.checked_duration_since(draw_start);
-                            let dur = bi.checked_duration_since(*ai);
-                            if let (Some(aedge), Some(bedge), Some(dur)) = (aedge, bedge, dur) {
-                                let aidx = (aedge.as_secs_f64() / bucket_size) as usize;
-                                let bidx = (bedge.as_secs_f64() / bucket_size) as usize;
-                                if aidx == bidx {
-                                    if let Some((_, v)) = buckets.get_mut(bidx) {
-                                        *v += diff as f64 / bucket_size;
-                                        maxv = maxv.max(*v);
-                                    }
-                                } else {
-                                    let rate = diff as f64 / dur.as_secs_f64();
-                                    if let Some((_, v)) = buckets.get_mut(aidx) {
-                                        *v += rate * ((aidx + 1) as f64 - aedge.as_secs_f64() / bucket_size);
-                                        maxv = maxv.max(*v);
-                                    }
-                                    if aidx + 1 <= bidx - 1 {
-                                        for (_, v) in buckets[aidx + 1 ..= bidx - 1].iter_mut() {
-                                            *v += rate;
-                                            maxv = maxv.max(*v);
-                                        }
-                                    }
-                                    if let Some((_, v)) = buckets.get_mut(bidx) {
-                                        *v += rate * (bedge.as_secs_f64() / bucket_size - bidx as f64);
-                                        maxv = maxv.max(*v);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    (topic, buckets, total)
-                })
-                .filter(|(_, _, total)| *total != 0)
-                .collect::<Vec<_>>();
+            let (maxv, data) = scraper.rates(interval, buckets);
             if maxy < maxv || maxy > 1.5 * maxv {
                 maxy = maxv * 1.25;
             }
             // TODO: color most active
-            let mut colors = vec![Color::White, Color::Blue, Color::Yellow, Color::Red, Color::Green, Color::Magenta, Color::Cyan];
+            let mut colors = vec![
+                Color::White,
+                Color::Blue,
+                Color::Yellow,
+                Color::Red,
+                Color::Green,
+                Color::Magenta,
+                Color::Cyan,
+            ];
             let data = data
                 .iter()
                 .map(|(topic, padata, _total)| {
@@ -167,11 +197,17 @@ pub(crate) fn run(opts: &Opts) -> Result<()> {
                         .bounds([-interval.as_secs_f64(), 0.0])
                         .labels(
                             once(Span::from(""))
-                                .chain((1..=width)
-                                    .step_by(date_length + space)
-                                    .map(|i| Span::from(chrono::Duration::from_std(interval.mul_f64(1. - i as f64 / width as f64))
-                                        .map(|dur: chrono::Duration| format_time(now_date - dur, long_time))
-                                        .unwrap_or("X".repeat(date_length)))))
+                                .chain((1..=width).step_by(date_length + space).map(|i| {
+                                    Span::from(
+                                        chrono::Duration::from_std(
+                                            interval.mul_f64(1. - i as f64 / width as f64),
+                                        )
+                                        .map(|dur: chrono::Duration| {
+                                            format_time(now_date - dur, long_time)
+                                        })
+                                        .unwrap_or("X".repeat(date_length)),
+                                    )
+                                }))
                                 .collect(),
                         ),
                 )
@@ -183,7 +219,9 @@ pub(crate) fn run(opts: &Opts) -> Result<()> {
                         .labels(
                             (0..=height / 10)
                                 .map(|p| {
-                                    Span::from(format_number(p as f64 / (height / 10) as f64 * maxy))
+                                    Span::from(format_number(
+                                        p as f64 / (height / 10) as f64 * maxy,
+                                    ))
                                 })
                                 .collect(),
                         ),
@@ -203,8 +241,21 @@ pub(crate) fn run(opts: &Opts) -> Result<()> {
 
 fn format_time(time: DateTime<Local>, long: bool) -> String {
     match long {
-        false => format!("{:02}:{:02}:{:02}", time.hour(), time.minute(), time.second()),
-        true => format!("{:04}-{:02}-{:02} {:02}:{:02}:{:02}", time.year(), time.month(), time.day(), time.hour(), time.minute(), time.second())
+        false => format!(
+            "{:02}:{:02}:{:02}",
+            time.hour(),
+            time.minute(),
+            time.second()
+        ),
+        true => format!(
+            "{:04}-{:02}-{:02} {:02}:{:02}:{:02}",
+            time.year(),
+            time.month(),
+            time.day(),
+            time.hour(),
+            time.minute(),
+            time.second()
+        ),
     }
 }
 
@@ -283,7 +334,7 @@ enum Message {
     },
 }
 
-#[derive(Debug,PartialEq,Eq,PartialOrd,Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct QueryTask {
     when: Instant,
     topic: String, // TODO: intern
@@ -320,18 +371,20 @@ fn query_offsets(state: Arc<State>, tx: mpsc::SyncSender<Message>, client: Clien
         }
 
         let mut bads = state.bad_brokers.lock().expect("poisoned").clone();
-        for QueryTask { when, topic, partition, leader } in query_tasks.range(..) {
+        for QueryTask {
+            when,
+            topic,
+            partition,
+            leader,
+        } in query_tasks.range(..)
+        {
             if bads.contains(leader) {
                 continue;
             }
             if let Some(sleep) = when.checked_duration_since(Instant::now()) {
                 thread::sleep(sleep);
             }
-            match client.fetch_watermarks(
-                topic,
-                *partition,
-                state.query_interval,
-            ) {
+            match client.fetch_watermarks(topic, *partition, state.query_interval) {
                 Ok((_low, high)) => tx.send(Message::PartitionOffsets {
                     now: Instant::now(),
                     topic: topic.into(),
