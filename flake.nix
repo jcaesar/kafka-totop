2769648b039493e632569efbe6343d1e09a661c5
Julius Michaelis <michaelis@fujitsu.com>
2024-05-29 15:43:06 +0900

    nix flake for building multiarch images

---
+++ flake.nix
@@ -0,0 +1,108 @@
+{
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs.lib) genAttrs listToAttrs sourceByRegex;
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "armv7l-linux"
+        "riscv64-linux"
+      ];
+      forAllSystems = genAttrs supportedSystems;
+      mk = localSystem: crossSystem: rec {
+        systems = (
+          if localSystem == crossSystem then
+            { system = localSystem; }
+          else
+            { inherit crossSystem localSystem; }
+        );
+        pkgs = (import nixpkgs systems).pkgsMusl;
+        main = pkgs.callPackage (
+          {
+            rustPlatform,
+            rdkafka,
+            buildPackages,
+          }:
+          rustPlatform.buildRustPackage {
+            pname = "kafka-totop";
+            version = "0.1.0";
+            src = sourceByRegex ./. [
+              ".*rs$"
+              "^Cargo\\..*"
+            ];
+            nativeBuildInputs = [ buildPackages.pkg-config ];
+            buildInputs = [ rdkafka ];
+            doCheck = false;
+            env.CARGO_FEATURE_DYNAMIC_LINKING = "yes";
+            cargoLock.lockFile = ./Cargo.lock;
+            meta.mainProgram = "totop";
+          }
+        ) { };
+        image = pkgs.buildPackages.dockerTools.streamLayeredImage {
+          name = "kafka-totop";
+          contents = [ pkgs.cacert ];
+          config.Entrypoint = [ (pkgs.lib.getExe main) ];
+        };
+      };
+    in
+    {
+      packages = forAllSystems (localSystem: {
+        default = (mk localSystem localSystem).main;
+        mergedOciDir =
+          let
+            inherit (nixpkgs.lib) concatStringsSep mapAttrsToList getExe';
+            pkgs = import nixpkgs { system = localSystem; };
+            images = forAllSystems (crossSystem: (mk localSystem crossSystem).image);
+            regctl = getExe' pkgs.regctl "regctl";
+            ociDirs = mapAttrsToList (k: v: ''
+              ${v} >docker-${k}.tar
+              ${regctl} image import ocidir://oci-${k} docker-${k}.tar
+              rm docker-${k}.tar
+            '') images;
+            mergeRefs = mapAttrsToList (k: _: "--ref ocidir://oci-${k}") images;
+            env.nativeBuildInputs = [ pkgs.regctl ];
+            cmd = ''
+              ${concatStringsSep "\n" ociDirs}
+
+              ${regctl} index create ocidir://$out \
+                ${concatStringsSep " " mergeRefs}
+
+              echo You can now run: ${regctl} image copy ocidir://$out docker.io/foobar/kafka-totop
+            '';
+          in
+          pkgs.runCommand "merge-images" env cmd;
+      });
+      apps = forAllSystems (
+        localSystem:
+        listToAttrs (
+          map (remoteSystem: {
+            name = "stream-image-${remoteSystem}";
+            value = {
+              type = "app";
+              program = "${(mk localSystem remoteSystem).image}";
+            };
+          }) supportedSystems
+        )
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo
+              rustfmt
+              rust-analyzer
+              cargo-watch
+              rustc
+            ];
+            inputsFrom = [ (mk system system) ];
+          };
+        }
+      );
+    };
+}
